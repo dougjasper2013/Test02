5d389f22306f621e8d3ac7de36d94fdb8136337f
Douglas Jasper <doug.jasper@trios.com>
2024-04-21 20:05:19 -0400

    After Adding XCode Project

---
+++ Test02/Test02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test02
+//
+//  Created by Douglas Jasper on 2024-04-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
